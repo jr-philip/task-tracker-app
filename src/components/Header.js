30aabbb7aa3a24ae0e0e8f0f73a6b7cc5c406841
jr-philip <philipmwaura48@gmail.com>
2023-02-01 18:56:16 -0600

    events 'onClick'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 const Header = ({title}) => {
+    const onClick =() =>{
+        console.log('click')
+    }
   return (
     <header className='header'>
       <h1>{title}</h1>  
-      <Button color='green' text='Hello'/>
-          <Button color='red' text='Hello' />
-          <Button color='brown' text='Hello' />
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
