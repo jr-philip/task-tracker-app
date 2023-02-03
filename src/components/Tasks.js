77ae1d0123818cc8264c363ad0dc73724b3b6fc1
jr-philip <philipmwaura48@gmail.com>
2023-02-02 20:26:06 -0600

    useState

---
+++ src/components/Tasks.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState()
+  return (
+    <>
+      {tasks.map((task) => (
+      <h3 key={task.id}>{task.text}</h3>)
+      )}
+    </>
+  )
+}
+
+export default Tasks
