ad8ce08aa97586b58a540822306eee52c804bc8f
jr-philip <philipmwaura48@gmail.com>
2023-02-02 20:30:24 -0600

    state update

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,7 +1,26 @@
 import { useState } from "react"
 
 const Tasks = () => {
-  const [tasks, setTasks] = useState()
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'doctor appointment',
+      day: 'feb 5th at 2pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'metting at school',
+      day: 'feb 6th at 2pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'food shopping',
+      day: 'feb 5th at 4pm',
+      reminder: false,
+    }
+  ])
   return (
     <>
       {tasks.map((task) => (
